f284da14797e916bbd9db5cdf7edbd02d403f2f6
Jordi Martín <36531612e@institutmontilivi.cat>
2019-10-05 15:54:29 +0200

    creado menu juegos en master

--- jocs/Program.cs
+++ jocs/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace jocs
 {
-    class Program
+    class MenuJocs
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Tria:\n1. Solitari\n2. Age of empires\n3. Doom\n4. Warcraft");
+            int opcioEscollida = int.Parse(Console.ReadLine());
+            switch (opcioEscollida)
+            {
+                case 1:
+                    Console.WriteLine("El solitari es un joc de cartes");
+                    break;
+                case 2:
+                    Console.WriteLine("Age of empires es un joc d'estrategia ambientat en periodes historics");
+                    break;
+                case 3:
+                    Console.WriteLine("El doom es un joc de matar marcians");
+                    break;
+                case 4:
+                    Console.WriteLine("El warcraft es un joc d'estrategia pre d'orcs i elfs");
+                    break;
+                default:
+                    Console.WriteLine("Crec que t'has colat");
+                    break;
+            }
         }
     }
 }
